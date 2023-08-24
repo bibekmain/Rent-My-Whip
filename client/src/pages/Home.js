eaec5bb8b27febb8a4b83c8ad831eebad66d57e6
BibekM <bibekmbkb@gmail.com>
2023-08-23 20:59:09 -0700

    finished initial file setup for client and server directories, time to hack

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+//TODO: import required components and add them to the home page
+
+import React from "react";
+// import ProductList from "../components/ProductList";
+// import CategoryMenu from "../components/CategoryMenu";
+// import Cart from "../components/Cart";
+
+const Home = () => {
+  return (
+    <div className="container">
+      {/* <CategoryMenu />
+      <ProductList />
+      <Cart /> */}
+    </div>
+  );
+};
+
+export default Home;
