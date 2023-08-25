61c0eaf44317e2e22d4541d2a116c37ef16aed88
Ajjeroni <adanjeronimo213@gmail.com>
2023-08-24 20:34:06 -0700

    new Car.js

--- server/models/Car.js
+++ server/models/Car.js
@@ -1,34 +1,42 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-
 const carSchema = new Schema({
     //the price of the car
     price: {
         type: Number,
         required: true
     },
-
     //the make of the car
     make: {
         type: String,
         required: true,
     },
-
     //the model of the car
     model: {
         type: String,
         required: true
     },
-
     //the year the car was manufactured
     year: {
         type: Number,
         required: true
     },
-    description: {
-        type: string
+    //number of doors
+    doors: {
+        type: Number
+    },
+    //Air conditioning
+    ac: {
+        type: Boolean
+    },
+    //manual or automatic
+    transmission: {
+        type: String
+    },
+    //gas or diesels
+    fuel: {
+        type: String
     }
 });
-
 const Car = mongoose.model('Car', carSchema);
 module.exports = Car;
