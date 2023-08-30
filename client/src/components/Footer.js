76849c19d38c872083d12db4ed0b0018d8c0abba
priyanka vivek <priyaneonlove89@gmail.com>
2023-08-30 09:16:11 -0700

    Adding src folder

---
+++ client/src/components/Footer.js
@@ -0,0 +1,75 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footer-content">
+            <ul className="footer-content__1">
+              <li>
+                <span>CAR</span> Rental
+              </li>
+              <li>
+                contact us for any car rental needs at
+              </li>
+              <li>
+                <a href="tel:123456789">
+                  <i className="fa-solid fa-phone"></i> &nbsp; (123) -456-789
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="mailto: 
+                RentmyWhip@gmail.com"
+                >
+                  <i className="fa-solid fa-envelope"></i>
+                  &nbsp; RentmyWhip@gmail.com
+                </a>
+              </li>
+
+              {/* <li>
+                <a
+                  style={{ fontSize: "14px" }}
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://jephunneh.com/"
+                >
+                  Design by cjephuneh
+                </a>
+              </li> */}
+            </ul>
+
+            <ul className="footer-content__2">
+              <li>Questions</li>
+              <li>
+                <a href="#home">Github</a>
+              </li>
+            </ul>
+
+            {/* <ul className="footer-content__2">
+              <li>Working Hours</li>
+              <li>Mon - Fri: 9:00AM - 9:00PM</li>
+              <li>Sat: 9:00AM - 19:00PM</li>
+              <li>Sun: Closed</li>
+            </ul> */}
+
+            <ul className="footer-content__2">
+              <li>Subscription</li>
+              <li>
+                <p>Subscribe your Email address for latest news & updates.</p>
+              </li>
+              <li>
+                <input type="email" placeholder="Enter Email Address"></input>
+              </li>
+              <li>
+                <button className="submit-email">Submit</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
