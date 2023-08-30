f8819f2cc0afd1717c98f50ae39aa23b00435435
Ajjeroni <adanjeronimo213@gmail.com>
2023-08-30 10:21:53 -0700

    solved minor bug in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "../src/dist/styles.css";
-import About from "./Pages/About";
-import Home from "./Pages/Home";
-// import Register from "./Pages/Register";
-import Navbar from "../src/components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
-import Models from "./Pages/Models";
-import Contact from "./Pages/Contact";
+import Models from "./pages/Models";
+import Contact from "./pages/Contact";
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="models" element={<Models />} />
         <Route path="contact" element={<Contact />} />
-        {/* <Route path="register" element={<Register/>}/> */}
+        <Route path="register" element={<Register/>}/>
       </Routes>
     </>
   );
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import React, { useState } from 'react';
 import { Form, Button, Alert, Input } from 'react-bootstrap';
 
-import {useMutation}  from '@apollo/client';
+import { useMutation }  from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
