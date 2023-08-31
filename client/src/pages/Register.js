d9c74353754be3d39aba22098c468b9e9779a8c1
Ajjeroni <adanjeronimo213@gmail.com>
2023-08-31 16:04:12 -0700

    worked on register page in client side

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,73 +1,140 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+// import { useForm } from "react-hook-form";
 import Footer from "../components/Footer";
 import HeroPages from "../components/HeroPages";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { Form, Button, Alert } from "react-bootstrap";
 
 function Register() {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm();
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
-    const onSubmit = (data) => {
-        const userData = JSON.parse(localStorage.getItem(data.email));
-        if (userData) { // getItem can return actual value or null
-            if (userData.password === data.password) {
-                console.log(userData.name + " You Are Successfully Logged In");
-            } else {
-                console.log("Email or Password is not matching with our record");
-            }
-        } else {
-            console.log("Email or Password is not matching with our record");
-        }
-    };
-    return (
-        <>
-            <section className="register-page">
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const response = await addUser({
+        variables: { ...userFormData },
+      });
+
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+
+      // const { token, user } = await response.json();
+      console.log(response.data.addUser.token);
+      Auth.login(response.data.addUser.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
+  return (
+    <>
+      <section className="register-page">
         <HeroPages name="register" />
         <div className="container">
           <div className="register-div">
             <div className="register-div__text">
               <h2>REGISTER</h2>
-              <p>
-               Register or create your account here
-              </p>
-            <div className="register-div__form">
-                <form >
-                    <label>
-                      <b>USERNAME *</b>
-                      </label>
-                    <input type="text" placeholder="Enter Username" />
-                   
-                    <label>
-                        <b>EMAIL *</b>
-                    </label>
-                    <input type="email" placeholder="youremail@example.com" ></input>
+              <p>Register or create your account here</p>
+              <div className="register-div__form">
+                <Form
+                  noValidate
+                  validated={validated}
+                  onSubmit={handleFormSubmit}
+                >
+                  <Alert
+                    dismissible
+                    onClose={() => setShowAlert(false)}
+                    show={showAlert}
+                    variant="danger"
+                  >
+                    Something went wrong with your signup!
+                  </Alert>
+                  <label>
+                    <b>USERNAME *</b>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Enter Username"
+                    name="username"
+                    onChange={handleInputChange}
+                    value={userFormData.username}
+                  ></input>
 
-                    <label>
-                        <b>PASSWORD *</b>
-                    </label>
-                    <input type="password" placeholder="Eneter password"></input>
-                    <label>
-                        <b>CONFIRM PASSWORD *</b>
-                    </label>
-                    <input type="password" placeholder="Eneter password"></input>
+                  <label>
+                    <b>EMAIL *</b>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="youremail@example.com"
+                    name="email"
+                    onChange={handleInputChange}
+                    value={userFormData.email}
+                  ></input>
 
+                  <label>
+                    <b>PASSWORD *</b>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Enter password"
+                    name="password"
+                    onChange={handleInputChange}
+                    value={userFormData.password}
+                  ></input>
 
-                    <button type="submit">
-                        <i className="fa-solid fa-envelope-open-text"></i>&nbsp; REGISTER
-                        
-                    </button>
-                </form>
+                  <Button
+                    type="submit"
+                    disabled={
+                      !(
+                        userFormData.username &&
+                        userFormData.email &&
+                        userFormData.password
+                      )
+                    }
+                    variant="success"
+                  >
+                    <i className="fa-solid fa-envelope-open-text"></i>&nbsp;
+                    REGISTER
+                  </Button>
+                </Form>
+              </div>
             </div>
-</div>
-</div>
-
+          </div>
         </div>
-            <Footer />
-</section>
-        </>
-    );
+        <Footer />
+      </section>
+    </>
+  );
 }
 export default Register;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Car } = require("../models");
 const { signToken } = require("../utils/auth");
-const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
+// const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 
 //TODO: replace all the categories and products stuff with our own queries
 const resolvers = {
